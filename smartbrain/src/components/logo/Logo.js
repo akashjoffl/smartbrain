67aaac0a1ee7bcab1dac19e4e38823258f138e40
Akash Jeganath <akashjoffl@icloud.com>
2020-03-23 19:12:12 +0530

    Modified 'react-tilt to ''@type/react-tilt';'

--- smartbrain/src/components/logo/Logo.js
+++ smartbrain/src/components/logo/Logo.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Tilt from '@type/react-tilt';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 25 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner"> <span>👽</span> </div>
+            </Tilt>
         </div>
     );
 }
