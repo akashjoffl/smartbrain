d3a77d751929b2f8d7234af75167b9243787e352
Akash Jeganath <akashjoffl@icloud.com>
2020-03-28 01:55:12 +0530

    29.29 Building Our Components

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
     );
---
+++ smartbrain/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div >
+            <div className='white f3'>
+                {"Akash, your current rank is..."}
+            </div>
+            <div className='white f3'>
+                {"#4"}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
