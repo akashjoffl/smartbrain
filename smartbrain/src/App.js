6c36ce9471f1a994f1724b0b3445ace1103d8466
Akash Jeganath <akashjoffl@icloud.com>
2020-03-28 02:07:27 +0530

    249. Building Our Components

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const particlesOption = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 class App extends Component {
  render() {
   return (
     <div className="App">
+      <Particles className="particles"
+        params={particlesOption}
+      />
       <Navigation />
       <Logo />
       <Rank />
