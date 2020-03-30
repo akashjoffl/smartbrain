d06657121d330a082fda595ef626efc643d9755f
Akash Jeganath <akashjoffl@icloud.com>
2020-03-31 03:56:49 +0530

    251. Image Recognition API

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '7245a1d56bbb40f88236b18c451371f4'
+});
 
 const particlesOption = {
   particles: {
@@ -23,18 +29,23 @@ class App extends Component {
     super();
     this.state = {
       input: "",
+      imageUrl: ""
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   } 
 
   onButtonSumbit = () => {
+    this.setState({imageUrl: this.state.input});
     console.log("click");
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
-      function (response) {
-        // do something with response
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+      .then(
+        function(response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
       },
       function (err) {
         // there was an error
@@ -55,6 +66,7 @@ class App extends Component {
         onInputChange={this.onInputChange} 
         onButtonSumbit={this.onButtonSumbit}
         />
+      <FaceRecognition imageUrl={this.state.imageUrl} />
     </div>
     );
   }
