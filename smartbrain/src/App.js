37409ad99e78e8051613fc6b0f604e406563386a
Akash Jeganath <akashjoffl@icloud.com>
2020-04-13 04:05:53 +0530

    256. Sign In Form And Routing
    25:14 Navigation fixes

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -33,7 +33,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'Signin'
+      route: 'Signin',
+      isSignedIn: false
     }
   }
 
@@ -71,6 +72,11 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
@@ -80,7 +86,7 @@ class App extends Component {
       <Particles className="particles"
         params={particlesOption}
       />
-      <Navigation onRouteChange={this.onRouteChange}/>
+      <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
       { this.state.route === 'home'
         ? <div>
           <Logo />
